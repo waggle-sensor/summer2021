61694b03682f65f635d73dbb0db8e58daeb66426
thejeffreyli <jkli@emory.edu>
2021-06-08 15:28:25 -0400

    demos update

--- Li/opencv_demos/basic_image_operations.py
+++ Li/opencv_demos/basic_image_operations.py
@@ -1,14 +1,18 @@
+'''
+Sources: 
+OpenCV Python Tutorial For Beginners 10
+https://www.youtube.com/watch?v=wSjB_iaQ1wE&list=PLS1QulWo1RIa7D1O6skqDQ-JZ1GGHKK-K&index=11
+
+
+'''
 import numpy as np
 import cv2
-
-#events = [i for i in dir(cv2) if 'EVENT' in  i]
-#print(events)
 
 
 # read images
 messi = cv2.imread('messi5.jpg')
 
-
+# image properties
 # print(messi.shape) # (342, 548, 3)
 # print(messi.size) # 562248
 # print(messi.dtype) # uint8
@@ -16,7 +20,7 @@ messi = cv2.imread('messi5.jpg')
 # split in bgr order: different channels
 b, g, r = cv2.split(messi)
 
-# cv2.imshow("Red", r)
+# cv2.imshow("Red", r) # shows only red
 # cv2.imshow("Green", b)
 # cv2.imshow("Blue", g)
 
@@ -24,15 +28,11 @@ b, g, r = cv2.split(messi)
 messi = cv2.merge((b,g,r))
 
 
-
-"photoshopping"
-
+# this is like photoshop
 # location of the ball
 ball = messi[280:340, 330:390]
-
 # where to place new ball
 messi[273:333, 100:160] = ball
-
 
 
 
@@ -47,18 +47,21 @@ logo = cv2.imread('opencv-logo.png')
 # cv2.imshow('image', messi)
 # cv2.imshow('image2', logo)
 
-"resize" # can only combine two photos if the size of images are the same 
+# resize: can only combine two photos if the size of images are the same 
 # image, new size
 messi = cv2.resize(messi, (512,512))
 logo = cv2.resize(logo, (512,512))
 
-"combine"
+# combine the two photos together
 # dst = cv2.add(messi, logo)
 
-# weighted add
+# weighted add: give a percentage value for each image
+# dst(I)=saturate(src1(I)∗alpha+src2(I)∗beta+gamma)
 dst = cv2.addWeighted(messi, 0.9 , logo, 0.1, 0)
 
 cv2.imshow('combination', dst)
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ Li/opencv_demos/object_detect_2.py
@@ -0,0 +1,62 @@
+'''
+VIDEO CAPTURE
+
+Sources: 
+OpenCV Python Tutorial For Beginners 13
+https://www.youtube.com/watch?v=3D7O_kZi8-o&list=PLS1QulWo1RIa7D1O6skqDQ-JZ1GGHKK-K&index=14
+More Readings:
+https://docs.opencv.org/master/df/d9d/tutorial_py_colorspaces.html
+https://www.pyimagesearch.com/2021/01/19/image-masking-with-opencv/
+
+
+'''
+
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+# from webcam source
+cap = cv2.VideoCapture(0);
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+while True:
+    #  video capture
+    _, frame = cap.read()
+
+    # converts bgr to hsv
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
